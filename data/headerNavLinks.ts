54c075ed397d82d236d1fc8ff82a523b4bec0acc
PhamVuThuNguyet <pvtnguyet.19it1vku@gmail.com>
2024-04-02 01:44:44 +0700

    first commit

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,8 @@
+const headerNavLinks = [
+  { href: '/', title: 'Home' },
+  { href: '/blogs', title: 'Blogs' },
+  { href: '/projects', title: 'Projects' },
+  { href: '/about', title: 'About' },
+]
+
+export default headerNavLinks
