1e232c5b91dc9fd93653176d12c08302bb1b1ca9
PhamVuThuNguyet <pvtnguyet.19it1vku@gmail.com>
2024-05-19 23:02:27 +0800

    fix: fix wrong import value after restructure

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,4 +1,4 @@
-import { TechStack } from '@/components/TechStack'
+import { TechStack } from 'app/projects/_components/TechStack'
 interface ProjectDataProps {
   title: string
   description: string
